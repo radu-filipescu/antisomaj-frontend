70af0d306207c10dc62ea04176ed20aaae0f125c
radu-filipescu <radu.filipescu@s.unibuc.ro>
2022-05-16 17:29:39 +0300

    new build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {HomepageComponent} from "./pages/homepage/homepage.component";
 import { JobsPageComponent } from './pages/jobs-page/jobs-page.component';
 import {LoginPageComponent} from "./pages/login-page/login-page.component";
 import {SignupPageComponent} from "./pages/signup-page/signup-page.component";
+import {HOME} from "@angular/cdk/keycodes";
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'home', component: HomepageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'signup', component: SignupPageComponent },
-  { path: 'job-search', component: JobsPageComponent}
+  { path: 'job-search', component: JobsPageComponent },
+  { path: '**', component: HomepageComponent }
 ];
 
 @NgModule({
