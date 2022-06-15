5aeaec987f53a3c442be302c88f388acc600b25f
radu-filipescu <radu.filipescu@s.unibuc.ro>
2022-06-15 23:43:03 +0300

    added some stuff on jobs service

--- src/app/pages/jobs-page/service/jobs-service.service.ts
+++ src/app/pages/jobs-page/service/jobs-service.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {JobDTO} from "../Entities/JobDTO";
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobsServiceService {
 
-  jobsApi: string = "https://localhost:8080/jobs"
+  jobsApi: string = "https://localhost:8080/jobs";
 
   constructor(private httpService: HttpClient) { }
 
   getJobs(){
-    return this.httpService.get(this.jobsApi)
+    return this.httpService.get<JobDTO>(this.jobsApi);
   }
 }
